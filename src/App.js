58bed903bec44d391a562d81e6be32a4681dd8ac
Jayden Spitulnik <81199186+JSS874@users.noreply.github.com>
2023-10-21 12:05:40 -0400

    basic chat app running

--- src/App.js
+++ src/App.js
@@ -1,9 +1,94 @@
+import { useState } from 'react'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
 
+const API_KEY = "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";
 
 function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am Dr. AI",
+      sender: "ChatGPT"
+    }
+  ])
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+    setTyping(true);
+
+    await processMessageToChatGPT(newMessages);
+  }
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessage = chatMessages.map((messageObject => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role="assistant";
+      }
+      else {
+        role = "user"
+      }
+      return {role: role, content: messageObject.message }
+    }));
+
+    const systemMessage = {
+      role: "system",
+      content: "You are pretending to be a doctor, you need to ask multiple questions such as what are my symptoms, how long have I had them, how old I am and any other necessary information you may need. You will then predict what I may have and I can do to feel better such as what medicine to take and any other advisable measures. Give short but concise responses and assume I have no medical knowledge"
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessage
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      console.log(data.choices[0].message.content);
+      setMessages(
+        [...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT"
+        }]
+      );
+      setTyping(false);
+    })
+  }
+
   return (
     <div className="App">
-      DR.AI
+      <div style={{ position: "relative", height:"800px", width:"700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList typingIndicator={typing ? <TypingIndicator content="Dr. AI is thinking" /> : null}>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
